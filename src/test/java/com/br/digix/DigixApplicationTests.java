4271cc41820b5eebdb6192ea3e5e7330986fad77
Leandro Arakaki <leanarakaki@gmail.com>
2023-03-21 16:35:52 -0400

    create a new project

---
+++ src/test/java/com/br/digix/DigixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.digix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
